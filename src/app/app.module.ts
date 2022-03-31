d1513d9f8bcd4acd5b937e70881c050ba0850501
Collo <mutaic84@gmail.com>
2022-03-31 13:43:23 +0300

    add new component to declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TeamCreateComponent } from './components/team-create/team-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamCreateComponent
   ],
   imports: [
     BrowserModule
